b33e53c41d91a86ce78c18c37abeed0ac99562de
Marcio Luiz <marcimpriori@gmail.com>
2015-09-25 07:47:33 -0300

    Aula 2
    Criação: View, Model, Resources.

---
+++ Treinamento Benner/App_Start/RouteConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Treinamento_Benner
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+
+            routes.MapRoute(
+                name: "Historico",
+                url: "Arquivo/{data}",
+                defaults: new { controller = "Historico", Action = "Arquivo" });
+
+
+            routes.MapRoute(
+                name: "Default",//route name
+                url: "{controller}/{action}/{id}",//url with parameters
+                defaults://parameter defaults
+                    new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+
+            
+        }
+    }
+}
---
+++ Treinamento Benner/Controllers/WelcomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Treinamento_Benner.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        //
+        // GET: /Welcome/
+
+        public string Index()
+        {
+            return "Hello World!";
+        }
+
+    }
+}
